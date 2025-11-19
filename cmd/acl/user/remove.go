eca51fff27867b2076da467a41d3b287776129e7
Namjae Kim <namjae.kim@namsic.dev>
2025-11-19 06:44:26 +0000

    INTERNAL: Check client_list before remove user

--- cmd/acl/user/remove.go
+++ cmd/acl/user/remove.go
@@ -1,7 +1,9 @@
 package user
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/go-zookeeper/zk"
 	"github.com/jam2in/arcusctl/internal"
@@ -28,6 +30,10 @@ var removeCmd = &cobra.Command{
 			panic(err)
 		}
 
+		if err := CheckClientList(conn, groupName, userName); err != nil {
+			panic(err)
+		}
+
 		if _, err := conn.Multi(
 			&zk.DeleteRequest{
 				Path:    internal.ZPATH_ACL_ROOT + "/" + groupName + "/" + userName + "/" + propName,
@@ -43,4 +49,48 @@ var removeCmd = &cobra.Command{
 
 		fmt.Println("OK")
 	},
+}
+
+func CheckClientList(conn *zk.Conn, groupName string, username string) error {
+	zpath := internal.ZPATH_ACL_MAPPING_ROOT
+	serviceCodes, _, err := conn.Children(zpath)
+	if err != nil {
+		return fmt.Errorf("Children(\"%s\"): %w", zpath, err)
+	}
+
+	for _, serviceCode := range serviceCodes {
+		zpath := internal.ZPATH_ACL_MAPPING_ROOT + "/" + serviceCode
+		group, _, err := conn.Get(zpath)
+		if err != nil {
+			return fmt.Errorf("Get(\"%s\"): %w", zpath, err)
+		}
+
+		if groupName != string(group) {
+			continue
+		}
+
+		zpath = internal.ZPATH_ROOT + internal.ZPATH_CLIENT_LIST + "/" + serviceCode
+		clientList, _, err := conn.Children(zpath)
+		if err != nil && !errors.Is(err, zk.ErrNoNode) {
+			return fmt.Errorf("Children(\"%s\"): %w", zpath, err)
+		}
+		for _, client := range clientList {
+			if strings.Contains(client, fmt.Sprintf("_sasluser=%s", username)) {
+				return fmt.Errorf("found %s in %s/%s", username, zpath, client)
+			}
+		}
+
+		zpath = internal.ZPATH_REPL_ROOT + internal.ZPATH_CLIENT_LIST + "/" + serviceCode
+		clientList, _, err = conn.Children(zpath)
+		if err != nil && !errors.Is(err, zk.ErrNoNode) {
+			return fmt.Errorf("Children(\"%s\"): %w", zpath, err)
+		}
+		for _, client := range clientList {
+			if strings.Contains(client, fmt.Sprintf("_sasluser=%s", username)) {
+				return fmt.Errorf("found %s in %s/%s", username, zpath, client)
+			}
+		}
+	}
+
+	return nil
 }
--- internal/const.go
+++ internal/const.go
@@ -1,5 +1,9 @@
 package internal
 
 const (
-	ZPATH_ACL_ROOT = "/arcus_acl"
+	ZPATH_ACL_ROOT         = "/arcus_acl"
+	ZPATH_ACL_MAPPING_ROOT = "/arcus_acl_mapping"
+	ZPATH_ROOT             = "/arcus"
+	ZPATH_REPL_ROOT        = "/arcus_repl"
+	ZPATH_CLIENT_LIST      = "/client_list"
 )
